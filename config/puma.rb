22d8577502de32d2ed0cb03b0d4085e52fdcd5fd
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2020-01-01 23:45:24 -0500

    remove puma state path

--- config/puma.rb
+++ config/puma.rb
@@ -49,7 +49,7 @@ root = "#{Dir.getwd}"
 
 bind "unix://#{root}/tmp/puma/socket"
 #pidfile "#{root}/tmp/puma/pid"
-state_path "#{root}/tmp/puma/state"
+#state_path "#{root}/tmp/puma/state"
 rackup "#{root}/config.ru"
 
 activate_control_app
