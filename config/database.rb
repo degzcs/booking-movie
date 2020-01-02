6d1ad0e5b9853be7c0021411a3bea6c705d30fca
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2020-01-01 23:33:40 -0500

    update sequel connect again

--- config/database.rb
+++ config/database.rb
@@ -1,11 +1,6 @@
 require 'sequel'
 
-DB = if ENV['RACK_ENV'] == 'production'
-        Sequel.connect("postgresql://#{ENV["POSTGRES_USER"]}:#{ENV["POSTGRES_PASSWORD"]}@#{ENV["POSTGRES_HOST"]}:#{ENV["POSTGRES_PORT"]}/#{ENV["POSTGRES_DB"]}")
-     else
-        Sequel.connect(ENV.fetch('DATABASE_URL'))
-     end
-
+DB = Sequel.connect(ENV.fetch('DATABASE_URL'))
 
 Dir["#{ APP_ROOT }/db/**/*.rb"].each { |file| require file }
 
